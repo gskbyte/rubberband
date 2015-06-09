90864554dafc97f87987e7b9600952d46b9912c5
Jose Alcalá Correa <jose.alcala.correa@gmail.com>
2015-06-09 13:56:12 -0700

    Initial Commit

---
+++ rubberband/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rubberband
+//
+//  Created by Jose Alcalá-Correa on 09/06/15.
+//  Copyright © 2015 XING. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
